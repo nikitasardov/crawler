7b6786acc4f21095e13cf649127ff939def2c8db
Sardov Nikita <nsardov@yandex.ru>
2017-02-04 17:06:39 +0400

    check pack of sites listed in input file

--- checkspell.php
+++ checkspell.php
@@ -62,29 +62,35 @@ function spellCheck ($urls) {
     $n = 0;
     $c = count($urls);
     $errurls = 0;
-    $rdir = prepareFName($sUrl);
+
     if (isset($argv[2])) {
         $logFile = $argv[2];
     } else $logFile =  'log__'.$rdir.'.txt';
     echo PHP_EOL;
-    `mkdir $rdir`;
     foreach ($urls as $u) {
         $n++;
         echo '-----'.PHP_EOL;
-        echo "$n из $c: $u".PHP_EOL;
-
+        echo "$n из $c: ";
+        if (strpos(trim($u),'http') != 0) {
+            echo PHP_EOL.'Invalid url skipped'.PHP_EOL;
+            `echo "ERROR! Invalid url skipped $u" >> results/$rdir/$logFile`;
+            $errurls++;
+            continue;
+        }
         if (addrAllowed($u) == false) {
             echo PHP_EOL.'Address skipped: '.$u.PHP_EOL.PHP_EOL.PHP_EOL;
+            `echo "Address skipped: $u" >> results/$rdir/$logFile`;
             $errurls++;
             continue;
         } else {
             //echo PHP_EOL.'Address ok: '.$u.PHP_EOL;
         }
+        echo $u.PHP_EOL;
 
-        `yaspeller --report console,html --find-repeat-words --ignore-latin $u 2>&1 | tee -a $rdir/$logFile`;
+        `yaspeller --report console,html --find-repeat-words --ignore-latin $u 2>&1 | tee -a results/$rdir/$logFile`;
 
         $nn = prepareFName($u);
-        $fn = "$rdir/yasp_$nn.html";
+        $fn = "results/$rdir/yasp_$nn.html";
         `mv yaspeller_report.html $fn`;
     }
     echo PHP_EOL."===============================";
@@ -93,8 +99,8 @@ function spellCheck ($urls) {
     echo PHP_EOL;
     echo PHP_EOL."Urls found in $argv[1]/sitemap.xml: $c";
     echo PHP_EOL."Error urls skipped: $errurls";
-    echo PHP_EOL."Results saved in folder ./$rdir/";
-    echo PHP_EOL . "Log file is here ./$rdir/$logFile";
+    echo PHP_EOL."Results saved in folder .results/$rdir/";
+    echo PHP_EOL . "Log file is here .results/$rdir/$logFile";
     echo PHP_EOL.PHP_EOL;
 }
 
@@ -147,7 +153,7 @@ function checkSite($siteurl) {
     global $logFile;
     $sUrl = $siteurl;
     $urls = getSitePages($siteurl);
-    $rdir = prepareFName($sUrl);
+    mkResultsDir();
     if ($urls == false) {
         echo PHP_EOL . "=====================================================";
         echo PHP_EOL . "ERROR! no $siteurl/sitemap.xml found";
@@ -157,8 +163,8 @@ function checkSite($siteurl) {
         if ($m == 1) {
             $m_errors[] = "ERROR: no sitemap found: $siteurl";
         }
-        `mkdir $rdir`;
-        `echo "ERROR! sitemap.xml not found in $siteurl" >> $rdir/$logFile`;
+        $logFile =  'log__'.$rdir.'.txt';
+        `echo "ERROR! sitemap.xml not found in $siteurl" >> results/$rdir/$logFile`;
     } else {
         spellCheck($urls);
     }
@@ -170,7 +176,7 @@ function checkSites($sitelist) {
     global $m_errors;
     global $sUrl;
     $sites = getSites($sitelist);
-    $rdir = prepareFName($sUrl);
+    $rdir = 'results/'.prepareFName($sUrl);
     if ($sites == false) {
         echo PHP_EOL . "=====================================================";
         echo PHP_EOL . "ERROR! no sites found in $sitelist";
@@ -199,7 +205,31 @@ function checkSites($sitelist) {
     echo PHP_EOL.PHP_EOL;
 }
 
+function mkResultsDir() {
+    global $rdir;
+    global $logFile;
+    global $sUrl;
+    $rdir = prepareFName($sUrl);
+    `mkdir results`;
+    //`cd results/`;
+    `mkdir results/$rdir`;
+    //`cd ../`;
+}
+
 system('clear');
+
+if (!isset($argv[1])) {
+    echo PHP_EOL . "=====================================================";
+    echo PHP_EOL . "no sites to check";
+    echo PHP_EOL . "=====================================================";
+    echo PHP_EOL . "command should be like this:";
+    echo PHP_EOL . "1. php checkspell.php http://sitename.com  #to check single site";
+    echo PHP_EOL . "2. php checkspell.php listOfSites.txt result.txt -m  #to check all sites in the list";
+    echo PHP_EOL;
+    echo PHP_EOL . "checkspell stopped";
+    echo PHP_EOL . PHP_EOL;
+    die;
+}
 
 if (isset($argv[3])&&$argv[3] == '-m') {
     $m = 1; //check a more than 1 site
